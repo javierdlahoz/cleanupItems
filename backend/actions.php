8aefe39d13222c8986f8afb2822a587bc1b0b3f6
Javier de la Hoz <javierdlahoz@gmail.com>
2014-05-09 16:48:48 -0500

    headers

--- backend/actions.php
+++ backend/actions.php
@@ -1,4 +1,6 @@
 <?php 
+    header('Access-Control-Allow-Origin: *'); 
+    header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept');
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     ini_set('log_errors', 1);
